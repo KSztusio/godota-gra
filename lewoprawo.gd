485d9051a42a64df4725d259062e82fbd369cef7
Vivi <shoter998@gmail.com>
2022-03-31 19:00:21 +0200

    created bullet

--- lewoprawo.gd
+++ lewoprawo.gd
@@ -1,9 +1,5 @@
 extends SpringArm
 export var max_rotation_speed = 0.5
 func _process(delta):
-	var roty = 0
-	roty = Input.get_action_strength("right") - Input.get_action_strength("left")
-	if Input.get_action_strength("right") and rotation_degrees.y > -90:
-		rotate_y(-roty * delta * max_rotation_speed)	
-	if Input.get_action_strength("left") and rotation_degrees.y < 90:
-		rotate_y(-roty * delta * max_rotation_speed) 
+	pass
+#	roty = Input.get_action_strength("right") - Input.get_action_strength("left")
--- oslufa.gd
+++ oslufa.gd
@@ -1,10 +1,22 @@
 extends SpringArm
 export var max_rotation_speed = 0.5
+
+var bullet = preload("res://pocisk.tscn")
+var bullet_speed = 100
+
 func _process(delta):
 	var rotz = 0
+	var end_pos = $"KinematicBody/koniec"
 	rotz = Input.get_action_strength("up") - Input.get_action_strength("down")
 	if Input.get_action_strength("up") and rotation_degrees.z < -20:
 		rotate_z(rotz  * delta * max_rotation_speed)	
 	if Input.get_action_strength("down") and rotation_degrees.z > -90:
 		rotate_z(rotz  * delta * max_rotation_speed) 
+	
+	if Input.is_action_just_pressed("shot"):
+		var bullet_instance = bullet.instance()
+		bullet_instance.global_transform =  end_pos.global_transform
+		print(rotation_degrees.x)
+		bullet_instance.apply_impulse(Vector3.ZERO, Vector3(bullet_speed,0, rotation_degrees.y).rotated(end_pos.rotation, 1))
+		get_tree().get_root().add_child(bullet_instance)
 
