e33961a302d06ab12d3703caa6cf7073e7850dcf
KSztusio <st.lesner@gmail.com>
2022-03-31 22:33:52 +0200

    kw

--- pocisk.gd
+++ pocisk.gd
@@ -8,11 +8,12 @@ func _physics_process(delta):
 	var vz = 0
 	if done == 0:
 		vy = sin(PI/2 + rotation.z) * speed
-		vpoziom = cos(rotation.z) * speed
+		vpoziom = cos(PI/2 + rotation.z) * speed
 		vx = cos(rotation.y) * vpoziom
 		vz = -sin(rotation.y) * vpoziom
 		apply_impulse(Vector3.ZERO, Vector3(vx, vy, vz))
 		done = 1
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
